f10ec01ec5df19c09e7dee76a0b9cc9282a3870f
MMII0220 <abdulloevmuhammadcon36@gmail.com>
2022-05-05 19:19:27 +0300

    15 lines of code

--- main.js
+++ main.js
@@ -1,30 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
-  let cir = document.querySelector('.circle'),
-    eff;
-
-  console.log(document.documentElement.clientWidth);
+  const cir = document.querySelector('.circle');
 
   cir.addEventListener('mouseenter', () => {
-    eff1 = Math.ceil(Math.random() * 700);
-    eff2 = Math.ceil(Math.random() * 500);
-
     switch (Math.floor(Math.random() * 2)) {
       case 0:
-        cir.style.top = eff1 + 30 + 'px';
-        console.log(cir.offsetTop);
+        cir.style.top = Math.ceil(Math.random() * 650) + 60 + 'px';
         break;
       case 1:
-        cir.style.left = eff2 + 30 + 'px';
-        console.log(cir.offsetBottom);
+        cir.style.left = Math.ceil(Math.random() * 450) + 60 + 'px';
         break;
-      /* case 2:
-        cir.style.right = eff + 30 + 'px';
-        console.log(cir.offsetRight);
-        break;
-      case 3:
-        cir.style.left = eff + 30 + 'px';
-        console.log(cir.offsetLeft);
-        break; */
     }
   });
 });
