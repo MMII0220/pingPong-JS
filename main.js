767751d4f628999a5b5f66676b9ecdf441277cc1
MMII0220 <abdulloevmuhammadcon36@gmail.com>
2022-06-23 20:27:45 +0300

    not overlowing

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ document.addEventListener('DOMContentLoaded', () => {
   cir.addEventListener('mouseenter', () => {
     switch (Math.floor(Math.random() * 2)) {
       case 0:
-        cir.style.top = Math.ceil(Math.random() * 650) + 60 + 'px';
+        cir.style.top = Math.ceil(Math.random() * 650) + 80 + 'px';
         break;
       case 1:
-        cir.style.left = Math.ceil(Math.random() * 450) + 60 + 'px';
+        cir.style.left = Math.ceil(Math.random() * 450) + 80 + 'px';
         break;
     }
   });
